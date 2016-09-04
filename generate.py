34598d8694e23d960927c43bc471181ad2dc86b4
Lutes <Daniel.Lutes@mail.mcgill.ca>
2016-09-04 14:00:41 -0400

    Added Paragraph content

--- generate.py
+++ generate.py
@@ -1,13 +1,29 @@
 # This file generates HTML for featured articles
 # 
 # Based on: http://www.nytimes.com/2016/03/20/magazine/the-fugitive.html?_r=0
-open("endpoints.txt", 'w').close()
+
+open("index.html", 'w').close()
 title = raw_input("Title: ")
 subtitle = raw_input("Subtitle: ")
+author = raw_input("Author: ")
+date = raw_input("Month Day, Year : ")
+date = date.upper()
+author = author.upper()
+
 headImageUrl = raw_input("Image Url: ")
+headImageSentence = raw_input("Image Sentence: ")
+headImageCredit = raw_input("Image Credit(Jon Tonks for The New York Times): ")
+logoColor = raw_input("Logo color: ")
 
+paragraphs = []
+with open("article.txt", "r") as article:
+	content = article.read()
+	content = content.splitlines()
+	for x in range(0, len(content)):
+		if (content[x] != ''):
+			paragraphs.append(content[x])
 
-with open("endpoints.txt", "a") as myfile:
+with open("index.html", "a") as myfile:
 	
 	#HEADER
 	myfile.write("<!DOCTYPE html>\n<html>\n\t<head>\n");
@@ -26,12 +42,31 @@ with open("endpoints.txt", "a") as myfile:
 	myfile.write("\t\t<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>\n");
 	myfile.write("\t\t<link rel=\"stylesheet\"	href=\"index.css\">\n");
 	myfile.write("\t\t<link rel=\"icon\" type=\"image/png\" href=\"http://mcgilltribune.com/wp-content/uploads/2015/09/favicon.ico\">\n");
-	
 
 	#GOOGLE ANALYTICS
 	myfile.write("\t\t<script>\n\t\t\t(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n\t\t\t(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n\t\t\tm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n\t\t\t})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\t\t\tga('create', 'UA-34387711-1', 'auto');\n\t\t\tga('send', 'pageview');\n\t\t</script>\n");
 
+	#OPEN BODY
 	myfile.write("\t</head>\n\t<body>\n");
-	myfile.write("\t</body>\n</html>");
+	
+	#PAGE TOP
+	#TODO Need a src for black and white. 
+	if((logoColor == 'Black')or(logoColor == 'black')):
+		myfile.write("\t\t<img  class='tribune-logo'/>\n");
+	else :
+		myfile.write("\t\t<img class='tribuneLogo'/>\n");
+	myfile.write("\t\t<img class='header-image' src='" + headImageUrl + "'/>\n");
+	myfile.write("\t\t<h1 class='title'>" + title + "</h1>\n");
+	myfile.write("\t\t<h3 class='subtitle'>" + subtitle + "</h3>\n");
+	myfile.write("\t\t<h5 class='byline'>By " + author +"   " + date + "</h5>\n");
+	myfile.write("\t\t<p class='header-image-sentance'>\n\t\t\t" + headImageSentence)
+	myfile.write("\n\t\t\t<i class='header-image-credit'>" + headImageCredit + "</i>\n")
+	myfile.write("\t\t</p>\n")
 
+	for i in range(0, len(paragraphs)):
+		myfile.write("\t\t<p class='content-paragraph'>\n")
+		myfile.write("\t\t\t" + paragraphs[i] + "\n")
+		myfile.write("\t\t</p>\n")
 
+	#CLOSE BODY
+	myfile.write("\t</body>\n</html>")
